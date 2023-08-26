335995f7c24ee40c7dad01db1c3612d14f8a3422
Grian Gajila <griangajila2@gmail.com>
2023-08-26 11:16:49 +0800

    wip

---
+++ src/pages/home.jsx
@@ -0,0 +1,195 @@
+import { useState } from 'react';
+import { Dialog } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+
+const navigation = [
+	{ name: 'Home', href: '#' },
+	{ name: 'Popular', href: '/popular' },
+	{ name: 'Destination', href: '/destination' },
+	{ name: 'Spot', href: '/spot' },
+	{ name: 'Blogs', href: '/blogs' },
+	{ name: 'About', href: '/about' },
+];
+
+export default function Home() {
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+	return (
+		<div className="bg-white w-full">
+			<header className="fixed bg-inherit inset-x-0 top-0 z-50">
+				<nav
+					className="flex items-center justify-between p-6 lg:px-8"
+					aria-label="Global"
+				>
+					<div className="flex lg:flex-1">
+						<a href="#" className="-m-1.5 p-1.5">
+							<img
+								className="h-8 w-auto"
+								src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+								alt=""
+							/>
+						</a>
+					</div>
+					<div className="mr-20">
+						<input
+							className="border-solid border-2 border-blue-500 rounded-md text-left font-medium text-gray-400 text-md p-2 outline-none h-9 hover:border-gray-400"
+							type="search"
+							placeholder="Search"
+							aria-label="Search"
+						/>
+					</div>
+					<div className="flex lg:hidden">
+						<button
+							type="button"
+							className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+							onClick={() => setMobileMenuOpen(true)}
+						>
+							<span className="sr-only">Open main menu</span>
+							<Bars3Icon className="h-6 w-6" aria-hidden="true" />
+						</button>
+					</div>
+					<div className="hidden lg:flex lg:gap-x-12">
+						{navigation.map((item) => (
+							<a
+								key={item.name}
+								href={item.href}
+								className="text-md font-semibold leading-6 text-gray-900 hover:text-blue-600"
+							>
+								{item.name}
+							</a>
+						))}
+					</div>
+					<div className="hidden lg:flex lg:flex-1 lg:justify-end">
+						<div className="hidden lg:flex m">
+							<button className="mr-8">
+								<svg
+									className="w-6 h-6"
+									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="currentColor"
+									viewBox="0 0 18 20"
+								>
+									<path d="M17.8 13.75a1 1 0 0 0-.859-.5A7.488 7.488 0 0 1 10.52 2a1 1 0 0 0 0-.969A1.035 1.035 0 0 0 9.687.5h-.113a9.5 9.5 0 1 0 8.222 14.247 1 1 0 0 0 .004-.997Z" />
+								</svg>
+							</button>
+
+							<a
+								href="#"
+								className="text-sm font-semibold leading-6 text-blue-600 font-sans"
+							>
+								Sign in <span aria-hidden="true">&rarr;</span>
+							</a>
+						</div>
+					</div>
+				</nav>
+				<Dialog
+					as="div"
+					className="lg:hidden"
+					open={mobileMenuOpen}
+					onClose={setMobileMenuOpen}
+				>
+					<div className="fixed inset-0 z-50" />
+					<Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+						<div className="flex items-center justify-between">
+							<a href="#" className="-m-1.5 p-1.5">
+								<span className="sr-only">Your Company</span>
+								<img
+									className="h-8 w-auto"
+									src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+									alt=""
+								/>
+							</a>
+							<button
+								type="button"
+								className="-m-2.5 rounded-md p-2.5 text-gray-700"
+								onClick={() => setMobileMenuOpen(false)}
+							>
+								<span className="sr-only">Close menu</span>
+								<XMarkIcon className="h-6 w-6" aria-hidden="true" />
+							</button>
+						</div>
+						<div className="mt-6 flow-root">
+							<div className="-my-6 divide-y divide-gray-500/10">
+								<div className="space-y-2 py-6">
+									{navigation.map((item) => (
+										<a
+											key={item.name}
+											href={item.href}
+											className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+										>
+											{item.name}
+										</a>
+									))}
+								</div>
+								<div className="py-6">
+									<a
+										href="#"
+										className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+									>
+										Sign in
+									</a>
+								</div>
+							</div>
+						</div>
+					</Dialog.Panel>
+				</Dialog>
+			</header>
+
+			<div className="relative isolate px-6 pt-14 lg:px-8">
+				<div
+					className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+					aria-hidden="true"
+				>
+					<div
+						className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+						style={{
+							clipPath:
+								'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+						}}
+					/>
+				</div>
+				<div className="mx-auto max-w-3xl py-32 sm:py-48 lg:py-20">
+					<div className="hidden sm:mb-20 sm:flex sm:justify-center">
+						<div className="relative rounded-full px-3 py-2 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+							Explore more beautiful traveling spot in philippines.{' '}
+							<a href="#" className="font-semibold text-indigo-600">
+								<span className="absolute inset-0" aria-hidden="true" />
+								View more <span aria-hidden="true">&rarr;</span>
+							</a>
+						</div>
+					</div>
+					<div className="text-center">
+						<h1 className="text-3xl mb-1 tracking-tight text-blue-500 font-sans sm:text-6xl">
+							Welcome to Philippines!{' '}
+						</h1>
+						<h2 className="font-medium">Keep calm and travel on.</h2>
+						<p className="mt-6 text-lg leading-8 text-gray-600">
+							You’ve landed in a country of over 7,000 islands with pristine
+							beaches, gorgeous mountains,
+						</p>
+						<div className="mt-10 flex items-center justify-center gap-x-6">
+							<a
+								href="#"
+								className="rounded-md bg-indigo-600 mt-10 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+							>
+								Explore Now
+							</a>
+						</div>
+					</div>
+				</div>
+				<div
+					className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+					aria-hidden="true"
+				>
+					<div
+						className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+						style={{
+							clipPath:
+								'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+						}}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
