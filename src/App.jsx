8979ffdec81fac15b8c54b7d2b6865e158455038
Grian Gajila <griangajila2@gmail.com>
2023-08-28 21:01:46 +0800

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './pages/home';
 
-const App = () => {
+export const App = () => {
 	return (
-		<div>
-			<Route path="./pages/home">
-				<Home />
-			</Route>
-		</div>
+		<Route path="/Home">
+			<Home />
+		</Route>
 	);
 };
-
-export default App;
